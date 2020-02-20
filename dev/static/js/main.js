8bae1916ef08055d80e07bf02534b919009c8f68
PavelSedoy <pavliksedoy@gmail.com>
2020-02-20 09:15:08 +0200

    How it work Desctop

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,48 @@
 $(document).ready(function () {
-    svg4everybody({});
+    // Content slider
+    var howContentSlider = new Swiper ('.how-desc-slider__container', {
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+            formatFractionCurrent: function(number) {
+                if (number < 10) {
+                    number = '0' + number;
+                }
+                return number;
+            },
+            formatFractionTotal: function(number) {
+                if (number < 10) {
+                    number = '0' + number;
+                }
+                return number;
+            },
+        },
+    
+        // Navigation arrows
+        navigation: {
+          nextEl: '.how-desc-slider__next-slide',
+          prevEl: '.how-desc-slider__prev-slide',
+        },
+    });
+    // End content slider
+
+    // Screens slider
+    var howScreenSlider = new Swiper ('.how-img-slider__container', {
+        slidesPerView: 1,
+        spaceBetween: 40,
+        allowTouchMove: false
+    });
+    // End screen slider
+
+    // Change screen on changing content slide
+    howContentSlider.on('slideChange', function() {
+        var toSlide = howContentSlider.activeIndex;
+        howScreenSlider.slideTo(toSlide);
+    });
+    // End change screen on changing content slide
 });
