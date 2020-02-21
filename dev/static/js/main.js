aa16e984e6322f3ad6e7d159e8007813ffe4354e
PavelSedoy <pavliksedoy@gmail.com>
2020-02-21 17:21:54 +0200

    p

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -50,4 +50,16 @@ $(document).ready(function () {
         howScreenSlider.slideTo(toSlide);
     });
     // End change screen on changing content slide
+
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault()
+      
+        $('html, body').animate(
+          {
+            scrollTop: $($(this).attr('href')).offset().top,
+          },
+          500,
+          'linear'
+        )
+      })
 });
